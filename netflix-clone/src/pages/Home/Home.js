cfd1198b579e5d4d853b423ddca5f507bb125904
YiheyisL <yoola7@gmail.com>
2024-01-23 15:17:55 +0300

    banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Footer from "../../components/Footer/Footer.";
 import Header from "../../components/Header/Header";
+import Banner from "../../components/Banner/Banner";
+import Footer from "../../components/Footer/Footer.";
 
 const Home = () => {
   return (
     <>
       <Header />
-
+      <Banner />
       <Footer />
     </>
   );
